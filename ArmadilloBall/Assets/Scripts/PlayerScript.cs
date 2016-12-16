ccd69620404ef4a7fbe8424d7a02519a9e2125a0
Danise Edra <daniseedra117@yahoo.com>
2016-12-15 17:28:55 -0800

    fixed timer

--- ArmadilloBall/Assets/Scripts/PlayerScript.cs
+++ ArmadilloBall/Assets/Scripts/PlayerScript.cs
@@ -14,6 +14,8 @@ public class PlayerScript : MonoBehaviour {
     private float startingSec, startingMin;
     private Scene currentScene;
 
+	public int totalPickups;
+
     // Use this for initialization
     void Start () {
         currentScene = SceneManager.GetActiveScene();
@@ -22,8 +24,8 @@ public class PlayerScript : MonoBehaviour {
         setCountText();
         clearLevelText.gameObject.SetActive(false);
  
-        startingSec = PlayerPrefs.GetInt("SecTime");
-        startingMin = PlayerPrefs.GetInt("MinTime");
+		startingSec = 0;//PlayerPrefs.GetInt("SecTime");
+		startingMin = 0;//PlayerPrefs.GetInt("MinTime");
     }
 
 	// Update is called once per frame
@@ -33,22 +35,20 @@ public class PlayerScript : MonoBehaviour {
 
     IEnumerator OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("pickUps"))
-        {
-            other.gameObject.SetActive(false);
-            count++;
-            setCountText();
-        }
-        else if (other.gameObject.CompareTag("goal"))
-        {
-            PlayerPrefs.SetInt("SecTime", (int)sec);
-            PlayerPrefs.SetInt("MinTime", (int)min);
-            PlayerPrefs.SetInt("CurrentScore", count);
-            clearLevelText.gameObject.SetActive(true);
-            float fadeTime = GameObject.Find("Stage").GetComponent<Fading>().BeginFade(1);
-            yield return new WaitForSeconds(fadeTime);
-            SceneManager.LoadScene(currentScene.buildIndex + 1);
-        }
+		if (other.gameObject.CompareTag ("pickUps")) {
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		} else if (other.gameObject.CompareTag ("goal")) {
+			PlayerPrefs.SetInt ("SecTime", (int)sec);
+			PlayerPrefs.SetInt ("MinTime", (int)min);
+			PlayerPrefs.SetInt ("CurrentScore", count);
+			clearLevelText.gameObject.SetActive (true);
+			float fadeTime = GameObject.Find ("Stage").GetComponent<Fading> ().BeginFade (1);
+			yield return new WaitForSeconds (fadeTime);
+			SceneManager.LoadScene (currentScene.buildIndex + 1);
+		}
+
         else if (other.gameObject.CompareTag("deadZone"))
         {
             reset();
@@ -57,8 +57,8 @@ public class PlayerScript : MonoBehaviour {
 
     void setTimeText()
     {
-        min = (int)(Time.timeSinceLevelLoad / 60f) + startingMin;
-        sec = (int)(Time.timeSinceLevelLoad % 60f) + startingSec;
+		min = (int)(Time.timeSinceLevelLoad / 60) + startingMin;
+		sec = (int)((Time.timeSinceLevelLoad + startingSec) % 60);
         timeText.text = min.ToString("00") + ":" + sec.ToString("00");
     }
 
