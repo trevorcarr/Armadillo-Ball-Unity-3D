57fe3eff4b488f8f09fcfb3a9de074eca5fd0c38
tcarr <tcarr@undergroundelephant.com>
2016-12-19 22:34:12 -0800

    added stuff

--- ArmadilloBall/Assets/Scripts/StageMovement.cs
+++ ArmadilloBall/Assets/Scripts/StageMovement.cs
@@ -8,8 +8,6 @@ public class StageMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
-=======
 
 	}
 
@@ -38,57 +36,3 @@ public class StageMovement : MonoBehaviour {
 		transform.rotation = Quaternion.Slerp(transform.rotation, xRotation, Time.deltaTime * smooth);
 	}
 }
-
-/*
-using UnityEngine;
-using System.Collections;
-
-public class StageMovement : MonoBehaviour {
-    public float smooth = 2.0F;
-    public float tiltAngle = 30.0F;
->>>>>>> 497a1285f493de995615063bea1d90684846000d
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
-	void FixedUpdate()
-	{
-		#if UNITY_STANDALONE || UNITY_EDITOR
-		float tiltAroundZ = -1*Input.GetAxis("Horizontal") * tiltAngle;
-		float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
-		#elif UNITY_ANDROID || UNITY_IOS || UNITY_WP_8_1
-		float tiltAroundX = Input.acceleration.x;
-		float tiltAroundZ = Input.acceleration.z;
-		#endif
-		// Gets the current frontward facing direction of the camera
-		Vector3 direction = Camera.main.transform.forward;
-
-
-		Quaternion zRotation = Quaternion.AngleAxis(tiltAroundZ + player.transform.position.y, direction);
-		Vector3 perpendicular = Quaternion.AngleAxis(90, transform.up) * direction;
-		Quaternion xRotation = Quaternion.AngleAxis(tiltAroundX, perpendicular);
-
-		transform.rotation = Quaternion.Slerp(transform.rotation, zRotation, Time.deltaTime * smooth);
-		transform.rotation = Quaternion.Slerp(transform.rotation, xRotation, Time.deltaTime * smooth);
-	}
-<<<<<<< HEAD
-}
-=======
-    void FixedUpdate()
-    {
-        #if UNITY_STANDALONE || UNITY_EDITOR
-            float tiltAroundZ = -1*Input.GetAxis("Horizontal") * tiltAngle;
-            float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
-        #elif UNITY_ANDROID || UNITY_IOS || UNITY_WP_8_1
-            float tiltAroundX = Input.acceleration.x;
-            float tiltAroundZ = Input.acceleration.z;
-        #endif
-		Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
-        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
-    }
-}
-*/
->>>>>>> 497a1285f493de995615063bea1d90684846000d
